260bb77adeaad2c91931a023fa9069acd42bec56
chava2708 <salvador95_2@hotmail.com>
2017-02-28 11:58:44 -0600

    Primer Ejemplo de DOM

---
+++ dom1/main.js
@@ -0,0 +1,5 @@
+//var cajas = document.getElementsByTagName('div');
+var cajas = document.getElementsByClassName('caja');
+
+cajas[0].textContent = 'Hola mundo'
+cajas[1].innerHTML = '<b>Hola javaScript</b>'
